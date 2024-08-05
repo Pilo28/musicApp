a2debff41f9b75738835636602d753575748327f
Pilo28 <pdefelitto@gmail.com>
2024-08-05 13:46:27 +0200

    added components

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -5,12 +5,22 @@ import { FeatureRoutingModule } from './feature-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CallbackComponent } from './pages/callback/callback.component';
 import { SharedModule } from '../shared/shared.module';
+import { NowPlayingComponent } from './components/now-playing/now-playing.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { SearchComponent } from './components/search/search.component';
+import { LibraryComponent } from './components/library/library.component';
+import { RecommendationsComponent } from './components/recommendations/recommendations.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    CallbackComponent
+    CallbackComponent,
+    NowPlayingComponent,
+    PlaylistComponent,
+    SearchComponent,
+    LibraryComponent,
+    RecommendationsComponent
   ],
   imports: [
     CommonModule,
