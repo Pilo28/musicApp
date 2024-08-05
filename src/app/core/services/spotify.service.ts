d145f92ec246a655e148f33ff186b758836be5a1
Pilo28 <pdefelitto@gmail.com>
2024-08-05 14:00:24 +0200

    update service

--- src/app/core/services/spotify.service.ts
+++ src/app/core/services/spotify.service.ts
@@ -1,7 +1,6 @@
 // spotify.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthService } from './auth.service';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -10,17 +9,11 @@ import { environment } from '../../../environments/environment';
 export class SpotifyService {
   private apiUrl = environment.apiUrl
 
-  constructor(private http: HttpClient, private authService: AuthService) {}
+  constructor(private http: HttpClient) {}
 
-  private getHeaders() {
-    return new HttpHeaders({
-      Authorization: `Bearer ${this.authService.getToken()}`,
-    });
-  }
 
   getUserProfile() {
-    return this.http.get(`${this.apiUrl}/me`, {
-      headers: this.getHeaders(),
-    });
+    return this.http.get(`${this.apiUrl}/me`);
   }
+
 }
