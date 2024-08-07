e2011a442c1bfd72b585c8762d2556555d93d2dc
Pilo28 <pdefelitto@gmail.com>
2024-08-07 12:49:52 +0200

    added button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, CardComponent]
+  exports: [HeaderComponent, CardComponent, ButtonComponent]
 })
 export class SharedModule { }
